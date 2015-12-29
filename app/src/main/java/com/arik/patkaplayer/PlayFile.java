482b252ac0f286b97f48876bf68e1f4e6ba12e2a
arikankainen <60932647+arikankainen@users.noreply.github.com>
2015-12-30 00:23:43 +0200

    ensimmäinen versio

---
+++ app/src/main/java/com/arik/patkaplayer/PlayFile.java
@@ -0,0 +1,38 @@
+package com.arik.patkaplayer;
+
+import android.media.MediaPlayer;
+import java.io.File;
+import java.io.IOException;
+
+public class PlayFile {
+
+    private MediaPlayer mp = new MediaPlayer();
+
+    public void Play(File mp3)
+    {
+        mp.reset();
+        try {
+            mp.setDataSource(mp3.getAbsolutePath());
+            mp.prepare();
+        }
+
+        catch (IOException e) {
+        }
+
+        mp.start();
+    }
+
+    public void Repeat(){
+        mp.pause();
+        mp.seekTo(0);
+        mp.start();
+    }
+
+    public void Pause() {
+        mp.pause();
+    }
+
+    public void Stop() {
+        mp.reset();
+    }
+}
