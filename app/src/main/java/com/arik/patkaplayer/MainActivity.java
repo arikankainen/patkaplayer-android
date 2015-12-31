829bb6f2b02115549521495f53f4f72487c98c03
arikankainen <60932647+arikankainen@users.noreply.github.com>
2015-12-31 12:24:35 +0200

    kaikki toimii, paitsi asetukset pitää tehdä uudestaan

--- app/src/main/java/com/arik/patkaplayer/MainActivity.java
+++ app/src/main/java/com/arik/patkaplayer/MainActivity.java
@@ -1,23 +1,65 @@
 package com.arik.patkaplayer;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.SystemClock;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.Date;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String date;
     private Boolean showBack;
+
+    private SeekBar volumeSeekbar = null;
+    private AudioManager audioManager = null;
+    private ArrayList folderNames = new ArrayList();
+    private String sdcardFolder;
+    private String curFolder;
+    private boolean isFileList = false;
+    private int listPosition = 0;
+    private int listPositionPixels = 0;
+    private boolean timerActive = false;
+    private int timerMin = 1;
+    private int timerMax = 10;
+    private Random rndDelay = new Random();
+    private PlayFile play = new PlayFile();
+    private String currentFolder;
+
+    private ArrayList<File> allFiles = new ArrayList<>();
+    private ArrayList<File> folderFiles = new ArrayList<>();
+    private Random rnd = new Random();
+    private Menu menu;
+    private static final String PREFS_NAME = "prefs";
+    private Handler handler = new Handler();
+    private Handler handler2 = new Handler();
+
+    // ****** OVERRIDE ****************************************************************************
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,39 +68,95 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        date = DateFormat.getDateTimeInstance().format(new Date());
         showBack = false;
 
         if (savedInstanceState != null) {
-            date = savedInstanceState.getString("date");
             showBack = savedInstanceState.getBoolean("showBack");
         }
+
+        try {
+            setVolumeControlStream(AudioManager.STREAM_MUSIC);
+            this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            initControls();
+
+            File sdcard = new File(Environment.getExternalStorageDirectory(), "Pätkät");
+            sdcardFolder = sdcard.getAbsolutePath();
+
+            File[] allFolders = sdcard.listFiles();
+            for (File folder : allFolders) {
+                if (folder.isDirectory()) {
+                    File[] files = folder.listFiles();
+                    for (File file : files) {
+                        if (file.isFile()) allFiles.add(file);
+                    }
+                }
+            }
+
+            for (File folder : allFolders) {
+                if (folder.isDirectory()) folderNames.add(folder.getName());
+            }
+
+            setFolders();
+        }
+        catch (Exception ex) {
+
+            final Context context = this;
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+            // set title
+            alertDialogBuilder.setTitle("Folder not accessible.");
+
+            // set dialog message
+            alertDialogBuilder
+                    .setMessage("Folder \"Pätkät\" not found or access is denied.\r\n\r\nMake sure storage permission is granted in app settings.")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // if this button is clicked, close
+                            // current activity
+                            MainActivity.this.finish();
+                        }
+                    });
+
+            // create alert dialog
+            AlertDialog alertDialog = alertDialogBuilder.create();
+
+            // show it
+            alertDialog.show();
+        }
+
     }
 
-    private void hideBackButton() {
-        showBack = false;
-        getSupportActionBar().setDisplayHomeAsUpEnabled(showBack);
+    @Override
+    protected void onStart() {
+        initControls();
+
+        super.onStart();
     }
 
-    private void showBackButton() {
-        showBack = true;
-        getSupportActionBar().setDisplayHomeAsUpEnabled(showBack);
+    @Override
+    protected void onResume() {
+        //readTimerPrefs();
+
+        super.onResume();
     }
 
     @Override
     public void onBackPressed() {
-        Intent i = new Intent(Intent.ACTION_MAIN);
-        i.addCategory(Intent.CATEGORY_HOME);
-        startActivity(i);
-    }
-
-    public void onClicked(View v){
-        showBackButton();
+        if (isFileList) {
+            setFoldersFade();
+        }
+        else {
+            //play.Stop();
+            //super.onBackPressed();
+            Intent i = new Intent(Intent.ACTION_MAIN);
+            i.addCategory(Intent.CATEGORY_HOME);
+            startActivity(i);
+        }
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putString("date", date);
         savedInstanceState.putBoolean("showBack", showBack);
     }
 
@@ -66,6 +164,9 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        this.menu = menu;
+        timerOff();
+
         return true;
     }
 
@@ -85,17 +186,298 @@ public class MainActivity extends AppCompatActivity {
         }
 
         else if (id == R.id.action_timer) {
-            Intent intent = new Intent(this, TimerActivity.class);
-            startActivity(intent);
+            if (timerActive) timerOff();
+            else timerOn();
 
             return true;
         }
 
         else if (id == android.R.id.home ){
-            hideBackButton();
+            setFoldersFade();
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        boolean changed = false, up = false, down = false;
+
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP)
+        {
+            changed = true;
+            up = true;
+        }
+
+        else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            changed = true;
+            down = true;
+        }
+
+        if (changed) {
+            volumeSeekbar = (SeekBar)findViewById(R.id.seekVolume);
+
+            int progress = volumeSeekbar.getProgress();
+            int maxVol = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+            if (up && progress < maxVol) progress++;
+            if (down && progress > 0) progress--;
+
+            volumeSeekbar.setProgress(progress);
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    // ****** METHODS *****************************************************************************
+
+    private void initControls()
+    {
+        try {
+            volumeSeekbar = (SeekBar)findViewById(R.id.seekVolume);
+            audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            volumeSeekbar.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+            volumeSeekbar.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+
+            volumeSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onStopTrackingTouch(SeekBar arg0) {
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar arg0) {
+                }
+
+                @Override
+                public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                }
+            });
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void hideBackButton() {
+        showBack = false;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(showBack);
+    }
+
+    private void showBackButton() {
+        showBack = true;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(showBack);
+    }
+
+    public void onRandomClicked(View v){
+
+        int i;
+        File mp3;
+
+        if (isFileList) {
+            i = rnd.nextInt(folderFiles.size());
+            mp3 = folderFiles.get(i);
+        }
+        else {
+            i = rnd.nextInt(allFiles.size());
+            mp3 = allFiles.get(i);
+        }
+
+        playFile(mp3);
+    }
+
+    public void onRepeatClicked(View v){
+        play.Repeat();
+    }
+
+    public void onStopClicked(View v){
+        play.Pause();
+    }
+
+    private void playFile(File mp3)
+    {
+        int lastIndex = mp3.getPath().lastIndexOf(File.separator);
+        String folderFull = mp3.getPath().substring(0, mp3.getPath().lastIndexOf(File.separator));
+
+        int lastIndex2 = folderFull.lastIndexOf(File.separator);
+        String folderFull2 = folderFull.substring(lastIndex2 + 1);
+
+        String name = mp3.getName().replace(".mp3", "");
+
+        TextView tf = (TextView)findViewById(R.id.txtPlayingFolder);
+        tf.setText(folderFull2);
+
+        TextView t = (TextView)findViewById(R.id.txtPlaying);
+        t.setText(name);
+
+        play.Play(mp3);
+    }
+
+    private void setFilesFade(String folder)
+    {
+        //SystemClock.sleep(200);
+
+        currentFolder = folder;
+        ListView list = (ListView) findViewById(R.id.listMp3);
+        list.animate()
+                .alpha(0f)
+                .setDuration(300)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        //mLoadingView.setVisibility(View.GONE);
+                        setFiles(currentFolder);
+
+                        ListView list = (ListView) findViewById(R.id.listMp3);
+                        list.animate()
+                                .alpha(1f)
+                                .setDuration(300)
+                                .setListener(null);
+
+                    }
+                });
+    }
+
+    private void setFiles(String folder)
+    {
+        File subtitle = new File(folder);
+        getSupportActionBar().setSubtitle(subtitle.getName());
+        //getSupportActionBar().setIcon(null);
+
+        showBackButton();
+        savePosition();
+
+        isFileList = true;
+        ArrayList fileNames = new ArrayList();
+        folderFiles.clear();
+
+        curFolder = folder;
+        File f = new File(folder);
+        File[] files = f.listFiles();
+
+        for (File file : files) {
+            fileNames.add(file.getName().replace(".mp3", ""));
+            folderFiles.add(file);
+        }
+
+        ArrayAdapter<String> mp3Adapter = new ArrayAdapter<String>(this, R.layout.custom_list_item_multiple_choice, fileNames);
+        final ListView mp3List = (ListView) findViewById(R.id.listMp3);
+        mp3List.setAdapter(mp3Adapter);
+
+        mp3List.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = ((TextView) view).getText().toString();
+
+                String fullname = combine(curFolder, item + ".mp3");
+                File mp3 = new File(fullname);
+                playFile(mp3);
+            }
+        });
+    }
+
+    private void setFoldersFade()
+    {
+        ListView list = (ListView) findViewById(R.id.listMp3);
+        list.animate()
+                .alpha(0f)
+                .setDuration(300)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        //mLoadingView.setVisibility(View.GONE);
+                        setFolders();
+
+                        ListView list = (ListView) findViewById(R.id.listMp3);
+                        list.animate()
+                                .alpha(1f)
+                                .setDuration(300)
+                                .setListener(null);
+
+                    }
+                });
+    }
+
+    private void setFolders()
+    {
+        getSupportActionBar().setSubtitle("");
+        //getSupportActionBar().setIcon(R.drawable.ppicon32);
+        hideBackButton();
+
+        isFileList = false;
+        ArrayAdapter<String> mp3Adapter = new ArrayAdapter<String>(this, R.layout.custom_list_item_multiple_choice, folderNames);
+        final ListView mp3List = (ListView) findViewById(R.id.listMp3);
+        mp3List.setAdapter(mp3Adapter);
+
+        restorePosition();
+
+        mp3List.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = ((TextView) view).getText().toString();
+
+                setFilesFade(combine(sdcardFolder, item));
+            }
+        });
+    }
+
+    private void savePosition()
+    {
+        ListView list = (ListView) findViewById(R.id.listMp3);
+        View v = list.getChildAt(0);
+        listPosition = list.getFirstVisiblePosition();
+        listPositionPixels = (v == null) ? 0 : (v.getTop() - list.getPaddingTop());
+    }
+
+    private void restorePosition()
+    {
+        ListView list = (ListView) findViewById(R.id.listMp3);
+        list.setSelectionFromTop(listPosition, listPositionPixels);
+    }
+
+    public static String combine (String path1, String path2)
+    {
+        File file1 = new File(path1);
+        File file2 = new File(file1, path2);
+        return file2.getPath();
+    }
+
+    private Runnable timerClip = new Runnable() {
+        public void run() {
+
+            if (timerActive) {
+                Button btn = (Button) findViewById(R.id.btnRandom);
+                btn.performClick();
+                int delay = rndDelay.nextInt(timerMax - timerMin) + timerMin;
+                handler.postDelayed(this, delay * 1000);
+            }
+        }
+    };
+
+    private void timerOn() {
+        MenuItem item = menu.findItem(R.id.action_timer);
+        item.getIcon().setAlpha(255);
+        readTimerPrefs();
+        timerActive = true;
+        int delay = rndDelay.nextInt(timerMax - timerMin) + timerMin;
+        handler.postDelayed(timerClip, delay * 1000);
+    }
+
+    private void timerOff() {
+        MenuItem item = menu.findItem(R.id.action_timer);
+        item.getIcon().setAlpha(40);
+        timerActive = false;
+    }
+
+    private void readTimerPrefs() {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        timerMin = Integer.valueOf(settings.getString("TimerMinDelay", "30"));
+        timerMax = Integer.valueOf(settings.getString("TimerMaxDelay", "120"));
+    }
+
 }
--- app/src/main/java/com/arik/patkaplayer/SettingsActivity.java
+++ app/src/main/java/com/arik/patkaplayer/SettingsActivity.java
@@ -1,12 +1,19 @@
 package com.arik.patkaplayer;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    public static final String PREFS_NAME = "prefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,38 @@ public class SettingsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        String audioFolder = settings.getString("AudioFolder", "");
+        String timerMinDelay = settings.getString("TimerMinDelay", "30");
+        String timerMaxDelay = settings.getString("TimerMaxDelay", "120");
+
+        TextView textAudioFolder = (TextView) findViewById(R.id.audioFolder);
+        textAudioFolder.setText(audioFolder);
+
+        TextView textTimerMinDelay = (TextView) findViewById(R.id.timerMinDelay);
+        textTimerMinDelay.setText(timerMinDelay);
+
+        TextView textTimerMaxDelay = (TextView) findViewById(R.id.timerMaxDelay);
+        textTimerMaxDelay.setText(timerMaxDelay);
+
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        TextView textAudioFolder = (TextView) findViewById(R.id.audioFolder);
+        TextView textTimerMinDelay = (TextView) findViewById(R.id.timerMinDelay);
+        TextView textTimerMaxDelay = (TextView) findViewById(R.id.timerMaxDelay);
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("AudioFolder", String.valueOf(textAudioFolder.getText()));
+        editor.putString("TimerMinDelay", String.valueOf(textTimerMinDelay.getText()));
+        editor.putString("TimerMaxDelay", String.valueOf(textTimerMaxDelay.getText()));
+
+        editor.commit();
     }
 
     @Override
